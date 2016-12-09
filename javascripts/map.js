c0ebfd6b7332f23c9fd61845ee1ba63354840370
Mitja Jež <mitja.jez@gmail.com>
2016-12-09 19:39:09 +0100

    Update map.js
    removed mapbox

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,10 +1,8 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'ciwi3woz1000z2tpgf1rw61ln',
-    accessToken: 'pk.eyJ1IjoibWl0amFqZXoiLCJhIjoiY2l3aTJ4ZmZqMDA1ajJ6bnlwbnVjbDR4MyJ9.0NQls3xnaGsI7NBXHvxULA'
+L.tileLayer('http://b.tile2.opencyclemap.org/transport/${z}/${x}/${y}.png', {
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 18
 }).addTo(mymap);
 
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
