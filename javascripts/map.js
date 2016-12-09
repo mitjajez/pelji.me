90b2a4c96e62dae628fb42edd4d2c464eda50629
Mitja Jež <mitja.jez@gmail.com>
2016-12-09 18:45:05 +0100

    Update map.js
    inserted public token

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,6 +1,6 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWl0amFqZXoiLCJhIjoiY2l3aTJ4ZmZqMDA1ajJ6bnlwbnVjbDR4MyJ9.0NQls3xnaGsI7NBXHvxULA', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'your.mapbox.project.id',
