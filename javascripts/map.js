9b071a59360f20911aae884ecb402ce80357a626
Mitja Jež <mitja.jez@gmail.com>
2016-12-09 19:42:34 +0100

    Update map.js
    copy - paste typo

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,6 +1,6 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('http://b.tile2.opencyclemap.org/transport/${z}/${x}/${y}.png', {
+L.tileLayer('http://b.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 18
 }).addTo(mymap);
